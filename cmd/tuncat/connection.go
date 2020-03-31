fabe696dffd0940bd9f46e5a807dbd46fbcdeee9
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2020-03-31 18:23:52 +0200

    Use a signle reader for the connection

--- cmd/tuncat/connection.go
+++ cmd/tuncat/connection.go
@@ -11,11 +11,12 @@ import (
 // ClientConnect do the tunnel connection negotiation sending the configuration parameters for the serveer
 // messages has to be echoed from the server in order to the connection to be established
 func ClientConnect(conn net.Conn, remoteNetwork, remoteGateway string) error {
+	reader := bufio.NewReader(conn)
 	// Send configuration to the server
 	text := fmt.Sprintf("remoteNetwork:%s", remoteNetwork)
 	conn.Write([]byte(text + "\n"))
 	// wait for acknowledge
-	message, _ := bufio.NewReader(conn).ReadString('\n')
+	message, _ := reader.ReadString('\n')
 	if strings.TrimSpace(message) != text {
 		return fmt.Errorf("Connection error, Sent: %s Received: %s", text, message)
 	}
@@ -23,7 +24,7 @@ func ClientConnect(conn net.Conn, remoteNetwork, remoteGateway string) error {
 	text = fmt.Sprintf("remoteGateway:%s", remoteGateway)
 	conn.Write([]byte(text + "\n"))
 	// wait for acknowledge
-	message, _ = bufio.NewReader(conn).ReadString('\n')
+	message, _ = reader.ReadString('\n')
 	if strings.TrimSpace(message) != text {
 		return fmt.Errorf("Connection error, Sent: %s Received: %s", text, message)
 	}
@@ -32,9 +33,10 @@ func ClientConnect(conn net.Conn, remoteNetwork, remoteGateway string) error {
 
 // ServerConnect do the tunnel connection negotiation
 func ServerConnect(conn net.Conn) (string, string, error) {
+	reader := bufio.NewReader(conn)
 	// Receive the configuration parameters
 	// will listen for message to process ending in newline (\n)
-	message, _ := bufio.NewReader(conn).ReadString('\n')
+	message, _ := reader.ReadString('\n')
 	// output message received
 	log.Printf("Message Received: %s", message)
 	// process for string received, we should receive the remoteNetwork parameter
@@ -46,7 +48,7 @@ func ServerConnect(conn net.Conn) (string, string, error) {
 	// send string back to client for ACK
 	conn.Write([]byte(message + "\n"))
 	// will listen for message to process ending in newline (\n)
-	message, _ = bufio.NewReader(conn).ReadString('\n')
+	message, _ = reader.ReadString('\n')
 	// output message received
 	log.Printf("Message Received: %s", message)
 	// process for string received, , we should receive the remoteGateway parameter
